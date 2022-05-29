b62a758234199b4a0953ff1dc07e05c5aab51c79
Marko <CroPsychooo@yahoo.com>
2022-05-29 02:32:49 +0200

    Added sounds and button "next question" on correct answer.

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("questions.json")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.questions[0].question); // just a test
-      var score = "0";
+      var score = 0;
+      var tracker = 0;
       var questionNum = -1;
       const btnStart = document.getElementById("start");
+      const btnGiveUp = document.getElementById("giveUp");
+      btnGiveUp.style = "display:none";
+
+      //////// SOUNDS:
+      var soundCorrect = new Audio("sounds/correct_answer.mp3");
+      var soundWrong = new Audio("sounds/sounds_wrong_answer.mp3");
+      var soundStart = new Audio("sounds/sounds_lets_play.mp3");
 
       btnStart.addEventListener("click", function nextQuestion() {
-        btnStart.remove();
+        btnStart.style = "display: none";
+        soundStart.play();
         ++questionNum; // increment question number (index) on each click event
 
         if (questionNum >= data.questions.length) {
@@ -44,23 +52,47 @@ document.addEventListener("DOMContentLoaded", function () {
             answerDivs[j].addEventListener("click", function checkAnswer(e) {
               if (e.target.id == answerCorrect) {
                 // this block will indicate if the answer was correct or not by changing bg color of answer clicked
+                soundCorrect.play();
                 e.target.style = "background-color:green";
                 answersContainer.style = "pointer-events:none";
               } else {
+                soundWrong.play();
                 e.target.style = "background-color:red;pointer-events:none";
                 answerDivs[answerCorrect].classList.add("answerCorrect");
-                console.log(answerDivs[answerCorrect]);
               }
               setTimeout(function () {
                 if (e.target.id == answerCorrect) {
-                  ++score;
+                  ++tracker;
+                  score += 100;
                   answersContainer.style = "";
-                  nextQuestion();
+                  //messy part where I copied the money part from original game
+                  questionNum == 0 ? (score = 100) : score;
+                  questionNum == 1 ? (score = 200) : score;
+                  questionNum == 2 ? (score = 300) : score;
+                  questionNum == 3 ? (score = 500) : score;
+                  questionNum == 4 ? (score = 1000) : score;
+                  questionNum == 5 ? (score = 2000) : score;
+                  questionNum == 6 ? (score = 4000) : score;
+                  questionNum == 7 ? (score = 8000) : score;
+                  questionNum == 8 ? (score = 16000) : score;
+                  questionNum == 9 ? (score = 32000) : score;
+                  questionNum == 10 ? (score = 64000) : score;
+                  questionNum == 11 ? (score = 125000) : score;
+                  questionNum == 12 ? (score = 250000) : score;
+                  questionNum == 13 ? (score = 500000) : score;
+                  questionNum == 14 ? (score = "1 Million") : score;
+                  btnStart.style = "display: block";
+                  btnStart.innerText = "Next Question";
+                  for(let n = 0; n < answerDivs.length; n++) {
+                    answerDivs[n].style = "pointer-events:none";
+                  }
+                  // repeat style for correct answer because it gets removed after timeout
+                  answerDivs[j].style = "background-color:green";
                 } else {
                   allContent.style.flexDirection = "column";
                   allContent.innerHTML = `<p>Wrong answer, the correct answer was:<br /><br /><strong>${answers[answerCorrect]}</strong></p>`;
                   allContent.innerHTML += `<h1>Game Over!</h1>`;
-                  allContent.innerHTML += `<p>You got ${score}/15 answers right!`;
+                  allContent.innerHTML += `<p>You got ${tracker}/15 answers right!`;
                   allContent.innerHTML += `<button id="restart" style="padding:1.5rem 2.5rem;font-size:1.25rem">Try again</button>`;
                   var btnRestart = document.getElementById("restart");
 
@@ -68,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     location.reload();
                   });
                 }
-              }, 1500);
+              }, 5000);
             }); // EventListener for answerDivs[j]
           } // for loop through answerDivs
         } // the app (else)
